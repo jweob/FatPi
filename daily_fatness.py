478743d2f728ea11607ce0562f60248498e29e53
jweob <jweob@cantab.net>
2018-01-21 12:49:15 -0500

    Added estimate if have not weighed self that day

--- daily_fatness.py
+++ daily_fatness.py
@@ -8,6 +8,9 @@ import numpy as np
 import schedule
 import time
 
+daily_weight_gain = 0.025  # In general gain about 25g per day once "off the wagon"
+
+
 def GetMeasures():
     with open('creds.pkl', 'rb') as input:
         creds = pickle.load(input)
@@ -20,6 +23,7 @@ def GetMeasures():
     measure_df = pd.DataFrame(measurelist, columns=["DateTime", "Weight", "Fat"])
     return measure_df
 
+
 def SendMail(msg):
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo()
@@ -27,9 +31,10 @@ def SendMail(msg):
     server.login(gmail_username, gmail_password)
 
     fromaddr = 'jweob1711@gmail.com'
-    toaddrs  = 'jweob1711@gmail.com'
+    toaddrs = 'jweob1711@gmail.com'
     server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
+
 
 def ProcessMeasures(measure_df):
     measure_df["Date"] = pd.to_datetime(measure_df["DateTime"].dt.date)
@@ -40,15 +45,30 @@ def ProcessMeasures(measure_df):
     measure_df = measure_df.set_index(["Date"])
 
     measure_df = measure_df.reindex(idx)
+    measure_df.insert(0, 'DayNumber', range(0, len(measure_df)))
+    last_weighed_record = measure_df[measure_df['DateTime'].notnull()].tail(1)
+
+    measure_df.insert(0, 'DaysSinceLastWeigh', measure_df['DayNumber'] - last_weighed_record['DayNumber'].tolist()[0])
     measure_df["WeightItp"] = measure_df.interpolate()["Weight"]
+    measure_df.loc[(measure_df["DaysSinceLastWeigh"] > 0), 'WeightItp'] = \
+        measure_df[measure_df["DaysSinceLastWeigh"] > 0]['WeightItp'] + \
+        measure_df[measure_df["DaysSinceLastWeigh"] > 0]['DaysSinceLastWeigh'] * daily_weight_gain
     measure_df["FatItp"] = measure_df.interpolate()["Fat"]
+    measure_df.loc[(measure_df["DaysSinceLastWeigh"] > 0), 'FatItp'] = \
+        measure_df[measure_df["DaysSinceLastWeigh"] > 0]['FatItp'] + \
+        measure_df[measure_df["DaysSinceLastWeigh"] > 0]['DaysSinceLastWeigh'] * daily_weight_gain
+
     return measure_df
+
 
 def AnalyseMeasures(measure_df):
     results = {}
-    results["WeightToday"] = measure_df.ix[-1, "Weight"]
+    results["WeightToday"] = measure_df.ix[-1, "WeightItp"]
     results["WeightGainRate"] = (measure_df.ix[-1, "WeightItp"] - measure_df.ix[-15, "WeightItp"]) / 2
+    results["LastWeighedRecord"] = measure_df[measure_df["DaysSinceLastWeigh"] == 0]
+    results["DaysSinceLastWeigh"] = measure_df.tail(1)["DaysSinceLastWeigh"].tolist()[0]
     return results
+
 
 def MakeMsg(results):
     subject = str("You weigh %.2f kg you fat pie" % results["WeightToday"])
@@ -56,6 +76,16 @@ def MakeMsg(results):
         body = str("You are currently gaining weight at %.2f kg per week" % results["WeightGainRate"])
     else:
         body = str("You are currently losing weight at %.2f kg per week" % -results["WeightGainRate"])
+    if results["DaysSinceLastWeigh"] > 0:
+        body += str(
+            "\nYou last weighed yourself %d days ago when you weighed %.2f. \n"
+            "At a weight gain of %.3f kg per day that means you probably now weigh %.2f kg" \
+            % (results["DaysSinceLastWeigh"],
+               results["LastWeighedRecord"]["Weight"].tolist()[0],
+               daily_weight_gain,
+               results["WeightToday"])
+            )
+
     message = 'Subject: {}\n\n{}'.format(subject, body)
     return message
 
@@ -70,7 +100,6 @@ def PieMail():
 
 
 if __name__ == "__main__":
-
 
     schedule.every().day.at("9:00").do(PieMail)
 
